e5fe54a1c2465ec1d415b3e7710b2dfb29d81320
Pradhnya <pradhnyashahu176@gmail.com>
2015-09-02 04:07:33 -0700

    Demo Instagram
    Instagram client

---
+++ app/src/main/java/com/glitterlabs/instagramclient/InstagramPhoto.java
@@ -0,0 +1,11 @@
+package com.glitterlabs.instagramclient;
+
+public class InstagramPhoto {
+
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+
+}
